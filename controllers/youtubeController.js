8e41d511a69897032b63d99c890a9ef54342ff85
Clément Huon <clemator22@gmail.com>
2016-04-06 05:51:12 +0200

    Improve youtube controller and view.

--- controllers/youtubeController.js
+++ controllers/youtubeController.js
@@ -1,49 +1,54 @@
-app.controller('youtubeController', ['$scope', function($scope) {
-	$scope.search = {input: "", results: 0, resultItem: []};
-	$scope.data = {
-		availableOptions: [
-			{name: '10', value: 10},
-			{name: '20', value: 20},
-			{name: '30', value: 30}
-    ],
-		selectedOption: {name: '10', value: 10}
-	};
-	console.log($scope.search.input);
-	$scope.apiKey = 'AIzaSyD6jgZkBrETrJhcnLqZ3IC0A27xQILaQ-U';
-	$scope.query;
+app.controller('youtubeController', ['$scope', '$http', '$window',
+	function($scope, $http, $window) {
+		$scope.search = {input: "", results: 0, resultItems: [], resultVideos: [], resultChannels: []};
+		$scope.data = {
+			availableOptions: [
+				{name: '10', value: 10},
+				{name: '20', value: 20},
+				{name: '30', value: 30}
+		],
+			selectedOption: {name: '10', value: 10}
+		};
+		$scope.apiKey = 'AIzaSyD6jgZkBrETrJhcnLqZ3IC0A27xQILaQ-U';
+		$scope.playVideo = false;
 
-	$scope.handleKey = function($event) {
-		if ($event.keyCode == 13) {
-			$scope.search();
-		}
-	}
-	$scope.search = function() {
-		if (!angular.isDefined($scope.search.input)) {
-			console.log($scope.search.input);
-			return;
-		}
+		$scope.handleKey = function($event) {
+			if ($event.keyCode == 13) {
+				$scope.search();
+			}
+		};
 		
-		$scope.query = $.ajax({
-			type: 'GET',
-			dataType: "json",
-			url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=" + $scope.data.selectedOption.value + "&order=relevance&q=" + $scope.search.input + "&key=" + $scope.apiKey
-		});
-		$scope.query.done(function() {
-			$("#search-results").replaceWith('<p id="search-results">Total results : ' + $scope.query.responseJSON.pageInfo.totalResults);
-			$("#search-video-result").replaceWith('<div id="search-video-result"></div>');
-			$.each($scope.query.responseJSON.items, function(i, item) {
-				if ($scope.query.responseJSON.items[i].id.kind == "youtube#channel")
-					$("#search-video-result").append("<div class='result-item'><span class='title'><strong>Channel :</strong> " + $scope.query.responseJSON.items[i].snippet.title + "</span></br>" + '<img id="channel' + $scope.query.responseJSON.items[i].id.channelId + '" src="' + $scope.query.responseJSON.items[i].snippet.thumbnails.default.url + '"/></br></div>');
-				else if ($scope.query.responseJSON.items[i].id.kind == "youtube#video")
-					$("#search-video-result").append("<div class='result-item'><span class='title'>" + $scope.query.responseJSON.items[i].snippet.title + "</br>" + '<img id="' + $scope.query.responseJSON.items[i].id.videoId + '" src="' + $scope.query.responseJSON.items[i].snippet.thumbnails.medium.url + '" style="cursor: pointer;"/></br></div>');
-			});
-			$("img").click(function(event) {
-				var id = event.target.id;
-				if (id.length == 11)
-					$(this).replaceWith('<iframe width="560" height="315" src="http://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>');
-			});
-			$("#search-video-result").show();
-			$("#search-results").show();
-		});
-	};
-}]);
+		$scope.search = function() {
+			if (!angular.isDefined($scope.search.input)) {
+				return;
+			}
+			$scope.search.results = 0;
+			$scope.search.resultItems = [];
+			$scope.search.resultChannels = [];
+			$scope.search.resultVideos = [];
+			
+			$http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=" + $scope.data.selectedOption.value + "&order=relevance&q=" + $scope.search.input + "&key=" + $scope.apiKey)
+				.then(function(res) {
+					$scope.search.results = res.data.pageInfo.totalResults;
+					$scope.search.resultItems = res.data.items;
+					
+					for (item of $scope.search.resultItems) {
+						if (item.id.kind == "youtube#channel")
+							$scope.search.resultChannels.push(item);
+						else if (item.id.kind == "youtube#video")
+							$scope.search.resultVideos.push(item);
+					}
+				});
+
+			// $(this).replaceWith('<iframe width="560" height="315" src="http://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>');
+		};
+		
+		$scope.play = function(id) {
+			console.log(id);
+		};
+		
+		$scope.goToChannel = function(id) {
+			$window.open('https://www.youtube.com/channel/' + id, '_blank');
+		};
+	}
+]);
